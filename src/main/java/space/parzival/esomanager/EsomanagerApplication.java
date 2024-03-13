35bd406d1d7c35d2808ba9d39250d123e0326dc6
Parzival <me@parzival.space>
2024-03-13 21:52:33 +0100

    new spring project

---
+++ src/main/java/space/parzival/esomanager/EsomanagerApplication.java
@@ -0,0 +1,13 @@
+package space.parzival.esomanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsomanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsomanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/space/parzival/esomanager/EsomanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package space.parzival.esomanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsomanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
